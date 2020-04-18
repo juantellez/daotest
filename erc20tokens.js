193fd04e1e5f4eaf3e7cb036f5435d09013bef57
Sebastián Téllez <juantellez@gootechnology.com>
2020-04-17 20:43:46 -0500

    Update erc20tokens.js

--- erc20tokens.js
+++ erc20tokens.js
@@ -1,11 +1,6 @@
 // mainnet-localhost
 
 window.widgetERC20Tokens = {
-  usdt: {
-    address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-    decimals: 6,
-    fullName: 'Usdt'
-  },
   // Symbol of ERC20 token in lowerCase
   btso: {
     // Address of ERC20 contract
@@ -15,19 +10,18 @@ window.widgetERC20Tokens = {
     // Display name in wallet (By default - its symbol of ERC20, but can be other userfriendy text)
     fullName: 'BTSO',
     // Icon of currency (image)
-    icon: 'https://growup.wpmix.net/wp-content/uploads/2016/10/favicon.png',
+    //icon: 'https://growup.wpmix.net/wp-content/uploads/2016/10/favicon.png',
     // Background color of icon
-    iconBgColor: '#ccc',
-  }
+    //iconBgColor: '#ccc',
+  },
 }
 
 window.buildOptions = {
-  ownTokens: true, // Will be inited from window.widgetERC20Tokens
+  ownTokens: false, // Will be inited from window.widgetERC20Tokens
   addCustomERC20: true, // Allow user add custom erc20 tokens
   curEnabled: { // Or 'false' if enabled all
     btc: true,
     eth: true,
-    btso: true,
   },
   invoiceEnabled: false, // Allow create invoices
 }
